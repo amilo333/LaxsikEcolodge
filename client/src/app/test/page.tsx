40c30b926a94512241b15207ab4c843f52ed9d70
amilo333 <132148898+amilo333@users.noreply.github.com>
2026-06-25 20:24:46 +0700

    Feat/create textarea component (#4)
    * feat/create-input-component
    
    * update
    
    * feat/update-input-component
    
    * update/create-textarea-component
    
    * update-textarea-component
    
    * update-textarea-component

--- client/src/app/test/page.tsx
+++ client/src/app/test/page.tsx
@@ -1,10 +1,10 @@
-import { Input, Textfield } from '@/components/core';
+import { Input, Textarea } from '@/components/core';
 
 export default function Test() {
   return (
     <div>
-      <Input label='Name' />
-      <Textfield label='Email' />
+      <Input label="Name" />
+      <Textarea label="Message" placeholder="Enter your message" />
     </div>
   );
 }
--- client/src/components/core/form/textfield/type.ts
+++ client/src/components/core/form/textfield/type.ts
@@ -3,10 +3,10 @@ import { FieldPath, FieldValues } from 'react-hook-form';
 import { TFieldState } from '../field/type';
 
 export type TTextfieldProps<
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TFieldValues /*đại điện cho dữ liệu form*/ extends FieldValues = FieldValues, //generic type 1
+  TName /*lấy các key hợp lệ*/ extends FieldPath<TFieldValues> = FieldPath<TFieldValues>, //generic type 2
 > = TFieldState<TFieldValues, TName> &
-  InputRootProps & {
+  InputRootProps & { //Nó chứa tất cả props của component Input.
     // Label for the text field
     label: string;
     // Type of the text field
