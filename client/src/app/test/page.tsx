eef7cef1f536ff1f4358cd407fa50e1c3530b235
amilo333 <locminh333@gmail.com>
2026-06-25 20:35:20 +0700

    update-dialog-component

--- client/src/app/test/page.tsx
+++ client/src/app/test/page.tsx
@@ -1,10 +1,9 @@
-import { Input, Textfield } from '@/components/core';
+import { Dialog, Input, Textfield } from '@/components/core';
 
 export default function Test() {
   return (
     <div>
-      <Input label='Name' />
-      <Textfield label='Email' />
+      <Dialog />
     </div>
   );
 }
