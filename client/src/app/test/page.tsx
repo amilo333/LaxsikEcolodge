9950bb85891ed162225c6c3c538902776f03951b
amilo333 <locminh333@gmail.com>
2026-06-29 22:36:45 +0700

    update-card-component

--- client/src/app/test/page.tsx
+++ client/src/app/test/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from '@/components/core';
+import { Card } from '@/components/core';
 
 export default function Test() {
   return (
     <div>
-      <Button>FIND NOW</Button>
+      <Card
+        imageSrc='./images/rooms/room.png'
+        title='DELUCY BACONY'
+        description='The spacious double room features air conditioning, a private entrance, a terrace with garden views as well as a private bathroom boasting a shower. The unit has 1 bed.'
+      />
     </div>
   );
 }
