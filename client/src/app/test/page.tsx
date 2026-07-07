643a4ba7203d4a4e5835a75075548566ef659847
amilo333 <locminh333@gmail.com>
2026-07-07 16:55:08 +0700

    update-footer-component

--- client/src/app/test/page.tsx
+++ client/src/app/test/page.tsx
@@ -1,16 +1,31 @@
-import { Select } from '@/components/core';
+import { Footer } from '@/components/layouts';
 
 export default function Test() {
   return (
     <div>
-      <Select
-        label='Guests'
-        options={[
-          { id: 'florida', label: 'Florida' },
-          { id: 'delaware', label: 'Delaware' },
-          { id: 'california', label: 'California' },
-          { id: 'texas', label: 'Texas' },
+      <Footer
+        logo='/images/logo.png'
+        title='Laxsik Ecolodge'
+        address='Lao Chai, Sa Pa, Lao Cai, Vietnam'
+        hotline='(+84) 214 3892 999'
+        email='info@laxsik.com or laxsik.customercare@gmail.com'
+        menus={[
+          'Join Our Teams',
+          'Become a Partner',
+          'Privacy & policies',
+          'Terms & Conditions',
         ]}
+        socials={[
+          { icon: '/images/icon/ic_twitter.png', alt: 'Twitter', href: '#' },
+          { icon: '/images/icon/ic_linkedin.png', alt: 'Linkedin', href: '#' },
+          { icon: '/images/icon/ic_facebook.png', alt: 'Facebook', href: '#' },
+          {
+            icon: '/images/icon/ic_instagram.png',
+            alt: 'Instagram',
+            href: '#',
+          },
+        ]}
+        copyright='© 2026  Laxsik Ecolodge. All rights reserved.'
       />
     </div>
   );
---
+++ client/src/components/layouts/footer/type.ts
@@ -0,0 +1,17 @@
+export type TSocialItem = {
+  icon: string;
+  alt: string;
+  href: string;
+};
+
+export type TFooterProps = {
+  logo: string;
+  title: string;
+  address: string;
+  hotline: string;
+  email: string;
+  menus: string[];
+  socials: TSocialItem[];
+  copyright?: string;
+  className?: string;
+};
