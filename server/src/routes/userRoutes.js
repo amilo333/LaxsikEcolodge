f9e311556ec6e2ef466b879869315c0a85431c85
amilo333 <locminh333@gmail.com>
2026-07-18 21:50:53 +0700

    update api user

---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import {
+  createUser,
+  deleteUserById,
+  getAllUsers,
+  getCurrentUserProfile,
+  getUserById,
+  loginUser,
+  logoutCurrentUser,
+  updateCurrentProfile,
+  updateUserById,
+} from "../controllers/userController.js";
+import { authenticate, authorizedAdmin } from "../middleware/authMiddleware.js";
+
+const router = Router();
+
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizedAdmin, getAllUsers);
+
+router.route("/auth").post(loginUser);
+router.route("/logout").post(logoutCurrentUser);
+
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentProfile);
+
+//ADMIN ROUTES 😎
+router
+  .route("/:id")
+  .delete(authenticate, authorizedAdmin, deleteUserById)
+  .get(authenticate, authorizedAdmin, getUserById)
+  .put(authenticate, authorizedAdmin, updateUserById);
+export default router;
--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,30 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
 const app = express();
-const cors = require("cors");
-// const route = require("./routes/ROUTES");
-// const db = require("./config/connectDB");
-require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-//Connect to database
-// db.connect();
+// Connect to database
+connectDB();
 
-//Run json in request.http
+// Parse request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-//Enable cors
+// Enable CORS
 app.use(cors());
 
-//Routes init
-// route(app);
+// Routes;
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
